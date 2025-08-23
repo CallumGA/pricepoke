78c43376349c35cdc2155265cfafb52e8a90c0df
callumanderson <callum.g.anderson@gmail.com>
2025-08-23 17:23:47 -0400

    list data pre-processing tasks

---
+++ src/preprocess/preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import datetime
+
+"""
+TODO: 1. Clean the data
+        - remove special characters
+        - drop rows that are 80% empty
+        - remove duplicate rows
+        - make sure dates are consistent
+        - normalize categorical values (e.g., "NM", "Near Mint", "near-mint" → "Near Mint")
+        - look for outliers via statistical methods to remove them
+        - ensure correct data types
+TODO: 2. Vectorize/encode the data
+TODO: 3. Feature engineering
+"""
