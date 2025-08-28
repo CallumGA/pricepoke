4e3789608dbfe8934e5348bd16484b6b8b951efa
callumanderson <callum.g.anderson@gmail.com>
2025-08-28 13:23:15 -0400

    start the NN classifier architecture

--- src/model/network.py
+++ src/model/network.py
@@ -6,10 +6,11 @@ import torch.nn as nn
 import torch.optim as optim
 from sklearn.preprocessing import StandardScaler
 
-# TODO: build the neural network classifier via pytorch
+input_csv_path = "/Users/callumanderson/Library/Mobile Documents/com~apple~CloudDocs/Documents/Documents - Callum’s Laptop/Masters-File-Repo/pytorch-learning/pricepoke/data/processed/pokemon_final_with_labels.csv"
+encoder_path = "/Users/callumanderson/Library/Mobile Documents/com~apple~CloudDocs/Documents/Documents - Callum’s Laptop/Masters-File-Repo/pytorch-learning/pricepoke/models/encoders/scaler.pkl"
 
 # load labeled csv
-data = pd.read_csv("/Users/callumanderson/Library/Mobile Documents/com~apple~CloudDocs/Documents/Documents - Callum’s Laptop/Masters-File-Repo/pytorch-learning/pricepoke/data/processed/pokemon_final_with_labels.csv")
+data = pd.read_csv(input_csv_path)
 
 # split features (x)
 features = data[[c for c in data.columns if c != "y"]]
@@ -21,15 +22,12 @@ targets = data[["y"]]
 features = features.to_numpy(dtype="float32")
 targets = targets.to_numpy(dtype="float32")
 
-# lets normalize further for a mean of 0 and std of 1 and save for later predictions
+# normalize for a mean of 0 and std of 1 and save for later predictions
 scaler = StandardScaler()
 features = scaler.fit_transform(features)
-joblib.dump(scaler, "/Users/callumanderson/Library/Mobile Documents/com~apple~CloudDocs/Documents/Documents - Callum’s Laptop/Masters-File-Repo/pytorch-learning/pricepoke/models/encoders/scaler.pkl")
-
+joblib.dump(scaler, encoder_path)
 
 # add feature matrix and target matrix to pytorch tensors
 X_tensor = torch.tensor(features)
 y_tensor = torch.tensor(targets)
 
-print(features.mean().mean())
-print(features.std().mean())
