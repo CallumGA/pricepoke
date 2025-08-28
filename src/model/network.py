22637d867306711048211d84b8236edfa919afdd
callumanderson <callum.g.anderson@gmail.com>
2025-08-28 11:23:22 -0400

    start the NN classifier architecture

--- src/model/network.py
+++ src/model/network.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
@@ -6,7 +7,7 @@ import torch.optim as optim
 # TODO: build the neural network classifier via pytorch
 
 # load labeled csv
-data = pd.read_csv("/Users/callumanderson/Documents/Documents - Callum’s Laptop/Masters-File-Repo/pytorch-learning/pricepoke/data/processed/pokemon_final_with_labels.csv")
+data = pd.read_csv("/Users/callumanderson/Library/Mobile Documents/com~apple~CloudDocs/Documents/Documents - Callum’s Laptop/Masters-File-Repo/pytorch-learning/pricepoke/data/processed/pokemon_final_with_labels.csv")
 
 # split features (x)
 features = data[[c for c in data.columns if c != "y"]]
@@ -14,4 +15,12 @@ features = data[[c for c in data.columns if c != "y"]]
 # split target (y)
 targets = data[["y"]]
 
-# reshape tensors?
+# convert the dataframe to numpy matrix to ensure shape is defined
+features = features.to_numpy(dtype="float32")
+targets = targets.to_numpy(dtype="float32")
+
+# add feature matrix and target matrix to pytorch tensors
+X_tensor = torch.tensor(features)
+y_tensor = torch.tensor(targets)
+
+print("loaded")
